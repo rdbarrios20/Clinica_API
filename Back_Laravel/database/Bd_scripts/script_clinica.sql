c3c696bb190eb89458304101582bac328326a606
Rafael <rdbarrios20@hotmail.com>
2021-01-18 11:22:08 -0500

    Cambios en el script de BD

--- Back_Laravel/database/Bd_scripts/script_clinica.sql
+++ Back_Laravel/database/Bd_scripts/script_clinica.sql
@@ -1,27 +1,189 @@
-SELECT * FROM clinica.servicios;
-INSERT INTO `clinica`.`servicios`
-(`id_servicio`,
-`nombre`,
-`created_at`,
-`updated_at`)
-VALUES
-(1,
-'uci',
-now(),
-now());
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 18, 2021 at 05:20 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
 
-INSERT INTO `clinica`.`servicios`
-(`id_servicio`,
-`nombre`,
-`created_at`,
-`updated_at`)
-VALUES
-(2,
-'urgencias',
-now(),
-now());
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-use clinica;
-select ingresos.id_paciente_admision,ingresos.id_servicio,servicios.nombre as nombre_servicio,ingresos.created_at
-from ingresos inner join servicios
-on ingresos.id_servicio = servicios.id_servicio;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clinica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingresos`
+--
+
+CREATE TABLE `ingresos` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_paciente_admision` int(11) DEFAULT NULL,
+  `identificacion_familiar` varchar(45) DEFAULT NULL,
+  `nombre_familiar` varchar(45) DEFAULT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ingresos`
+--
+
+INSERT INTO `ingresos` (`id`, `id_usuario`, `id_paciente_admision`, `identificacion_familiar`, `nombre_familiar`, `id_servicio`, `created_at`, `updated_at`) VALUES
+(17, 13, 12, NULL, NULL, 2, '2021-01-18 06:57:48', '2021-01-18 06:57:48'),
+(20, 16, 124585, '78992252', 'Hector', 1, '2021-01-18 08:25:24', '2021-01-18 08:25:24'),
+(23, 13, NULL, '12345789', 'Adolfo', 1, '2021-01-18 20:20:19', '2021-01-18 20:20:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `identificacion_usuario` int(11) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`identificacion_usuario`, `password`, `created_at`, `updated_at`) VALUES
+(5546565, '5546565', '2021-01-18 08:25:34', '2021-01-18 08:25:34'),
+(1065632580, '1065632580', '2021-01-18 06:57:48', '2021-01-18 06:57:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id_servicio` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `servicios`
+--
+
+INSERT INTO `servicios` (`id_servicio`, `nombre`, `created_at`, `updated_at`) VALUES
+(1, 'uci', '2021-01-15 19:41:53', '2021-01-15 19:41:53'),
+(2, 'urgencias', '2021-01-15 19:41:53', '2021-01-15 19:41:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `identificacion` int(11) NOT NULL COMMENT 'identification',
+  `nombres` varchar(45) NOT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `identificacion`, `nombres`, `telefono`, `direccion`, `created_at`, `updated_at`) VALUES
+(13, 1065632580, 'Rafael Barrios', '3004158084', 'cll17 #32-80', '2021-01-18 06:57:48', '2021-01-18 06:57:48'),
+(16, 5546565, 'Edgar', '45852255', 'cra 14 # 32-45', '2021-01-18 08:25:16', '2021-01-18 08:25:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ingreso_servicios_idx` (`id_servicio`),
+  ADD KEY `fk_ingresos_usuarios_idx` (`id_usuario`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`identificacion_usuario`);
+
+--
+-- Indexes for table `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `identificacion_UNIQUE` (`identificacion`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ingresos`
+--
+ALTER TABLE `ingresos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD CONSTRAINT `fk_ingreso_servicios` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ingresos_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `login`
+--
+ALTER TABLE `login`
+  ADD CONSTRAINT `fk_login_usuario` FOREIGN KEY (`identificacion_usuario`) REFERENCES `usuarios` (`identificacion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
